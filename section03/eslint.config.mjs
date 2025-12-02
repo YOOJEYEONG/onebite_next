ace36bc05bfb5e224dabdb9f20f5db31a41a3f42
YooJeYoung <jdngh@naver.com>
2025-12-02 22:08:15 +0900

    study : 앱라이터 서버컴포넌트, 클라이언트 컴포넌트

--- section03/eslint.config.mjs
+++ section03/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +13,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    // 이 부분이 추가됩니다!
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
---
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,10 @@
+import ClientComponent from "./client-component";
+
+export default function Home() {
+  return (
+    <div>
+      인덱스 페이지
+      <ClientComponent />
+    </div>
+  );
+}
