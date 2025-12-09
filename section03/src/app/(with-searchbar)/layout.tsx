df2fdc823de500faa5e3729222b6a57ca342307e
YooJeYoung <jdngh@naver.com>
2025-12-09 11:03:05 +0900

    study : 스타일링

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,6 +1,11 @@
+import { ReactNode } from "react";
 import Searchbar from "../../components/searchbar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <div>
       <Searchbar />
